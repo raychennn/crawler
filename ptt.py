aa8c6afa1e3862c13c56af83bad4fdad01e85733
Ray Chen <rayallen1996xd@gmail.com>
2021-07-07 00:19:29 +0800

    add url & write

--- ptt.py
+++ ptt.py
@@ -1,10 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+root_url = 'https://www.ptt.cc/bbs/Stock/'
 
 r = requests.get('https://www.ptt.cc/bbs/Stock/index.html')
 
 soup = BeautifulSoup(r.text, 'html.parser')
 spans = soup.find_all('div', class_ = "title")
 span = [s.text.strip() for s in spans]
-print(span)
+
+for span in spans:
+	url = root_url + span.find('a').get('href')
+	print(url)
+	print(span.text)
+
+with open('test.csv', 'w', encoding = 'utf-8-sig') as f:
+	for span in spans:
+		f.write(span.text + '\n' + url)
